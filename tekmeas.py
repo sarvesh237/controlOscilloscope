e5c1fae41d4ff0076a1156858cae81530b3a9e8e
sarvesh237 <25300607+sarvesh237@users.noreply.github.com>
2020-02-08 23:04:09 +0530

    Add files via upload

---
+++ tekmeas.py
@@ -0,0 +1,34 @@
+import visa
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+
+
+rm = visa.ResourceManager('@py')
+#print(rm.list_resources())
+scope = rm.open_resource('USB0::1689::872::C038299::0::INSTR')
+scope.write('*rst')
+scope.write('autoset execute')
+time.sleep(10)
+
+scope.write("MEASUrement:MEAS1:SOURCE CH1")
+scope.write("MEASUrement:MEAS1:TYPE FREQuency")
+scope.write("MEASUrement:MEAS1:STATE ON")
+
+scope.write("MEASUrement:MEAS2:SOURCE CH1")
+scope.write("MEASUrement:MEAS2:TYPE CRMS")
+scope.write("MEASUrement:MEAS2:STATE ON")
+time.sleep(10)
+frequency,rms = [],[]
+for i in range(200):
+    a = scope.query('measurement:meas1:value?')
+    b = scope.query('measurement:meas2:value?')
+    time.sleep(0.1)
+    frequency.append(float(a[:len(a)-1]))
+    rms.append(float(b[:len(b)-1]))
+    print(i)
+    
+plt.scatter(rms,frequency,label = "rms vs freq")
+plt.legend()
+plt.show()
